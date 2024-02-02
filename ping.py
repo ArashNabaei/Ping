6c1203f281a1a960a4707e8f96c2bf55a7b94109
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 11:16:24 +0330

    Add calculate_checksum method

---
+++ ping.py
@@ -0,0 +1,13 @@
+import socket
+import struct
+import time
+import sys
+
+def calculate_checksum(data):
+    # Calculate the checksum for the ICMP packet
+    checksum = 0
+    for i in range(0, len(data), 2):
+        checksum += (data[i] << 8) + data[i + 1]
+    checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    checksum = ~checksum & 0xFFFF
+    return checksum
