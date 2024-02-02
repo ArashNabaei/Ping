5f0baa5ef036384c7fbe72d214342113a58d09e3
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 11:35:53 +0330

    Remove an unused method

--- ping.py
+++ ping.py
@@ -11,25 +11,6 @@ def calculate_checksum(data):
     checksum = ~checksum & 0xFFFF
     return checksum
 
-def create_icmp_packet():
-    icmp_type = 8
-    icmp_code = 0
-    icmp_checksum = 0
-    icmp_id = 12345
-    icmp_seq = 1
-    data = b'Hello, ICMP!'
-
-    icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq)
-
-    checksum_data = icmp_header + data
-    icmp_checksum = calculate_checksum(checksum_data)
-
-    icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq)
-
-    icmp_packet = icmp_header + data
-
-    return icmp_packet
-
 def is_valid_ip(ip):
 
     try:
