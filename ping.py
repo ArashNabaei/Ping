5462e0e365a167bca87c70166415859a9d8cc52a
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 11:21:09 +0330

    Modify ip validation method

--- ping.py
+++ ping.py
@@ -31,9 +31,11 @@ def create_icmp_packet():
     return icmp_packet
 
 def is_valid_ip(ip):
+
     try:
         socket.inet_pton(socket.AF_INET, ip)
         return True
+        
     except socket.error:
         return False
 
