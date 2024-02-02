353994b35ca371aee27b43833bfd97c6e92ef7c2
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 11:23:24 +0330

    Set a sequence number as input of create icmp method

--- ping.py
+++ ping.py
@@ -40,24 +40,19 @@ def is_valid_ip(ip):
         return False
 
 def create_icmp_packet(seq_num):
-    # Craft an ICMP packet with a simple structure
-    icmp_type = 8  # ICMP Echo Request
+    icmp_type = 8
     icmp_code = 0
     icmp_checksum = 0
     icmp_id = 12345
     data = b'Hello, ICMP!'
 
-    # Create the ICMP header
     icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, seq_num)
 
-    # Calculate checksum for the header and data
     checksum_data = icmp_header + data
     icmp_checksum = calculate_checksum(checksum_data)
 
-    # Recreate the ICMP header with the correct checksum
     icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, seq_num)
 
-    # Combine the header and data to form the complete packet
     icmp_packet = icmp_header + data
 
     return icmp_packet
